f7d201e2aa4854aaf6bf6e4ff23d7086e8fbae23
brendaji <bji@wellesley.edu>
2015-12-11 10:15:18 -0500

    Begining Maze class and testing -JY

--- Maze.java
+++ Maze.java
@@ -8,23 +8,39 @@
   * Purpose: Constructs the Maze of Pusheen Donut Dash 
   ****************************************************************************/
 
+import java.util.*;
+import java.io.*;
+
 public class Maze{
   // Constants:
   private final int INIT_CAPACITY = 20;
   
   // Instance Variables:
-  private int[] yEdLabels;
+  private int n; // number of Paodes
+  private String[] yEdLabels;
   private LinkedList<Paode> maze;
   private Paode beginning;
   private Paode home;
+  
   
   /* Constructor:
    * 
    */
   public Maze(String tgf_file_name){
     try{
+      n = 0;
+      yEdLabels = new String[INIT_CAPACITY];
+      Scanner scan = new Scanner (new File(tgf_file_name));
       
+      while (!scan.next().equals("#")){
+        // Need to expand Array...
+        yEdLabels[n] = scan.next();
+        n++;
+      }
       
+    }
+    catch (FileNotFoundException exception){
+      System.out.println("File not found.");
     }
   }
   
@@ -32,13 +48,17 @@ public class Maze{
    * @return String version of Object
    */
   public String toString(){
-    String result = "";
-    
+    String result = "[";
+    for (int i  = 0; i < n; i++){
+      result += (yEdLabels[i] + "  " );
+    }
+    result += "]";
     return result;
   }
   
   public static void main (String[] args){
-    
+    Maze m = new Maze("testingDiningHallFruit.tgf");
+    System.out.println(m);
   }
   
   
