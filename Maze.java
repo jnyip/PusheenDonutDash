6e1e5cf94cfb04c62abbecf07285e9bc0eb38088
Jamie Yip <jnyip96@gmail.com>
2015-12-11 09:45:20 -0500

    Renaming Maze to MazeGUI. Creating Maze in Maze

--- Maze.java
+++ Maze.java
@@ -1,30 +1,45 @@
-//********************************************************************
-// Maze.java Java Foundations
-//
-// Demonstrates key events.
-//********************************************************************
-import javax.swing.JFrame;
-import javax.swing.*;
-import java.io.*;
-import javax.imageio.*;
+/******************************************************************************
+  * Maze.java
+  * Pusheen Donut Dash - Maze Object Class
+  * 
+  * CS 230 Fall 2015 Final Project
+  * Written by: Jamie Yip
+  * Date: December 10, 2015
+  * Purpose: Constructs the Maze of Pusheen Donut Dash 
+  ****************************************************************************/
 
-public class Maze
-{
-//-----------------------------------------------------------------
-// Creates and displays the application frame.
-//-----------------------------------------------------------------
-  public static void main (String[] args)
-  {
-    JFrame frame = new JFrame ("Maze");
-    
-    frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-    
-
-    
-    frame.getContentPane().add (new MazePanel());
-    
-    frame.setResizable(true);
-    frame.pack();
-    frame.setVisible(true);
+public class Maze{
+  // Constants:
+  private final int INIT_CAPACITY = 20;
+  
+  // Instance Variables:
+  private int[] yEdLabels;
+  private LinkedList<Paode> maze;
+  private Paode beginning;
+  private Paode home;
+  
+  /* Constructor:
+   * 
+   */
+  public Maze(String tgf_file_name){
+    try{
+      
+      
+    }
   }
+  
+  /* toString() - Formats the Object for easy reading 
+   * @return String version of Object
+   */
+  public String toString(){
+    String result = "";
+    
+    return result;
+  }
+  
+  public static void main (String[] args){
+    
+  }
+  
+  
 }
